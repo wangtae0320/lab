643bbd4144fa54b7d0101d8c4b9568f1b00857d8
wangtae <wangtae0320@gmail.com>
2023-08-18 19:31:33 +0900

    maria db 연동

--- server/database/mariaDB.js
+++ server/database/mariaDB.js
@@ -5,6 +5,7 @@ const connect = maria.createConnection({
   host: process.env.HOST_URL,
   user: process.env.USER_NAME,
   password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 
 module.exports = connect;
